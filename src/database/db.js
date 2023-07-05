db762f6b35441be390fa06989fbd69f44ba81de8
¨pedroj4567¨ <pedroj4567@gmail.com>
2023-07-04 21:03:40 -0400

    .

--- src/database/db.js
+++ src/database/db.js
@@ -4,13 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 export const sequelize = new Sequelize(
-  `${process.env.DB_NAME}`,
-  `${process.env.DB_USER}`,
-  `${process.env.DB_PASSWORD}`,
+  `${process.env.URI_POSTGRES}`,
   {
-    host: `${process.env.DB_HOST}`,
     dialect:
       "postgres",
+      dialectOptions: {
+        ssl: true
+      }
   }
 );
 
