c5466094c7a3dba87f9036757852372ed51c1e20
¨pedroj4567¨ <pedroj4567@gmail.com>
2023-07-05 20:00:05 -0400

    .

--- src/database/db.js
+++ src/database/db.js
@@ -3,6 +3,19 @@ import dotenv from 'dotenv';
 //varibles de entornos
 dotenv.config()
 
+// export const sequelize = new Sequelize(
+//   `${process.env.URI_POSTGRES}`,
+//   {
+//     dialect: "postgres",
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       }
+//     }
+//   }
+// );
+
 export const sequelize = new Sequelize(
   `${process.env.URI_POSTGRES}`,
   {
